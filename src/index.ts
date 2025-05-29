a0bf6e380dc637642f520ae00b813dfea5f1c9a4
nizhantiw <nizhantiw@gmail.com>
2025-05-29 18:11:04 +0530

    added component of Layout

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,20 @@ export { Table, TableHeader, TableBody, TableRow, TableCell } from './components
 export { Switch } from './components/switch';
 export { Toast } from './components/toast';
 
+// Export layout components
+export { default as AppLayout } from './components/layout/AppLayout';
+export { default as AppHeader } from './components/layout/AppHeader';
+export { default as AppSidebar } from './components/layout/AppSidebar';
+export { default as SidebarWidget } from './components/layout/SidebarWidget';
+export { default as Backdrop } from './components/layout/Backdrop';
+
+// Export header components
+export { default as HeaderHomePage } from './components/header/HeaderHomePage';
+export { default as UserDropdown } from './components/header/UserDropdown';
+export { default as NotificationDropdown } from './components/header/NotificationDropdown';
+
+// Export context
+export { SidebarProvider, useSidebar } from './context/SidebarContext';
+
 // Export styles
 export { initStyles } from './styles';
